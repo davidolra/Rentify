15a06717839291941a9455e07311b537136e2e01
davidolra <da.olaver@duocuc.cl>
2025-09-17 18:33:39 -0300

    Delete userservice directory

--- userservice/src/main/kotlin/com/rentify/userservice/repository/RolRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.rentify.userservice.repository
-
-import com.rentify.userservice.model.Rol
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RolRepository : JpaRepository<Rol, Long>
