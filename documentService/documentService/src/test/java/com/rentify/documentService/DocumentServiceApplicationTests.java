d84f325fdfd1b84b120202028bbcebef56500684
davidolra <da.olaver@duocuc.cl>
2025-09-24 19:52:24 -0300

    se agrega nuevo microservicio de documentos

---
+++ documentService/documentService/src/test/java/com/rentify/documentService/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentify.documentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
