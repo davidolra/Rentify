001d5763bf7ef9a42242c14aedeb266d03b07255
davidolra <da.olaver@duocuc.cl>
2025-12-02 05:05:24 -0300

    eliminar archivos duplicados

--- documentService/src/main/java/com/rentify/documentService/repository/DocumentTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rentify.documentService.repository;
-
-import com.rentify.documentService.model.DocumentType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentTypeRepository extends JpaRepository<DocumentType, Long> {
-}
